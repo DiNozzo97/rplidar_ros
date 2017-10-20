97dd60d70b524e4da7b57120b148e18793faa1aa
Timbo Cole <timbo@timbotek.com>
2017-10-20 10:59:32 +0100

    Added stop on error

--- scripts/laserListener.py
+++ scripts/laserListener.py
@@ -2,6 +2,11 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=50)
+stop_command = Twist()
+
 
 def callback(data):
     range_min = data.range_min
@@ -17,14 +22,21 @@ def callback(data):
 
     overview_readings = [front_reading, left_reading, right_reading, rear_reading]
 
+    errorData = False
     for rId, reading in enumerate(overview_readings):
         if (reading > range_max) or (reading < range_min):
             overview_readings[rId] = "ERR"
+            cmd_vel_pub.publish(stop_command)
+            errorData = True
+
 
     rospy.loginfo('--------------------------------------------')
     rospy.loginfo('                     %s                     ', overview_readings[0])
     rospy.loginfo('%s                                        %s', overview_readings[1], overview_readings[2])
     rospy.loginfo('                     %s                     ', overview_readings[3])
+    if errorData :
+        rospy.loginfo('-----ROBOT STOPPED DUE TO ERROR IN DATA-----')
+
 
 def listener():
 
@@ -34,8 +46,8 @@ def listener():
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('laserListener', anonymous=True)
-
     rospy.Subscriber('scan', LaserScan, callback)
+
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
